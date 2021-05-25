ab6a16a81c301f15579717310757e2cf629b649b
Travis Siebenhaar <tsiebenhaar@Traviss-MacBook-Air.local>
2021-05-25 14:57:54 -0600

    - Add redux   - Add mock example(to be removed later)

--- App.js
+++ App.js
@@ -3,17 +3,21 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Provider } from "react-redux";
+import store from "./store";
 import LoginScreen from "./screens/LoginScreen/LoginScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
